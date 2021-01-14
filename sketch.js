61c44a8b09ee1c6bf4d14d822f6815d83c6b7ef9
isaiahaiasi <isaiah.althouse@gmail.com>
2021-01-14 00:37:21 -0700

    Let every pixel change color on mouseover event

--- sketch.js
+++ sketch.js
@@ -8,9 +8,14 @@ root.style.setProperty("--grid-size", gridSize);
 const pixels = [];
 
 for(let i = 0; i < (gridSize * gridSize); i++) {
-  let shade = Math.random();
+  
   let pixel = document.createElement("div");
   pixel.setAttribute("class","pixel");
+
+  pixel.addEventListener("mouseenter", e => {
+    let shade = Math.random() * 100;
+    e.target.style.backgroundColor = `hsl(40, 50%, ${shade}%`;
+  });
   etchContainer.appendChild(pixel);
   pixels.push(pixel);
 }
