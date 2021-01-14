75b17a10611c885636e7c4874c2d94e295ac6baa
isaiahaiasi <isaiah.althouse@gmail.com>
2021-01-14 01:14:13 -0700

    Add removeEventListener to pixels so color changes only once

--- sketch.js
+++ sketch.js
@@ -7,15 +7,25 @@ root.style.setProperty("--grid-size", gridSize);
 
 const pixels = [];
 
+let setPixelColor = randomShade;
+
 for(let i = 0; i < (gridSize * gridSize); i++) {
-  
   let pixel = document.createElement("div");
   pixel.setAttribute("class","pixel");
 
-  pixel.addEventListener("mouseenter", e => {
-    let shade = Math.random() * 100;
-    e.target.style.backgroundColor = `hsl(40, 50%, ${shade}%`;
+  pixel.addEventListener("mouseenter", setPixelColor);
+
+  pixel.addEventListener("mouseleave", e => {
+    pixel.removeEventListener("mouseenter", setPixelColor);
   });
+
   etchContainer.appendChild(pixel);
   pixels.push(pixel);
+  
 }
+
+function randomShade(e) {
+  let shade = Math.random() * 100;
+  e.target.style.backgroundColor = `hsl(40, 50%, ${shade}%`;
+}
+
