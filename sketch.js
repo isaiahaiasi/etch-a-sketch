f04bdf75caed98c34c99abc75e1d48dab8cd888f
isaiahaiasi <isaiah.althouse@gmail.com>
2021-01-15 00:51:43 -0700

    Deleted obsolete function for draw mode 'darken'

--- sketch.js
+++ sketch.js
@@ -36,9 +36,6 @@ drawModeButtons.forEach((btn)=> {
     case "random":
       drawFunction = totallyRandomColor;
       break;
-    case "darken":
-      drawFunction = chiaroscuro;
-      break;
     default:
       console.log(`Draw Mode option button set to invalid value ${btn.value}`, btn);
       return;
@@ -70,6 +67,7 @@ function updateColorPreview(previewBox) {
       `rgba(${rgbaCustom[0]},${rgbaCustom[1]},${rgbaCustom[2]},${rgbaCustom[3]})`;
 }
 
+// THE ACTUAL GRID
 function generateGrid(size) {
   while (etchCtn.firstChild) {
     etchCtn.removeChild(etchCtn.firstChild);
@@ -119,29 +117,6 @@ function totallyRandomColor() {
   let g = Math.random() * 256;
   let b = Math.random() * 256;
   return `rgb(${r},${g},${b})`;
-}
-
-// The only reason I pass the event to these functions...
-// TODO: I will probably have to delete this, because if I have blend modes,
-//    then the draw mode should be agnostic of the color beneath it.
-function chiaroscuro(e) {
-  let color = window.getComputedStyle(e.target, null).getPropertyValue("background-color");
-  let rgb = color.match(/([0-9.])+/gi);
-  rgb = [(+rgb[0]), (+rgb[1]), (+rgb[2]), (+rgb[3])];
-  rgb[3] = isNaN(rgb[3]) ? 1 : rgb[3];
-  let rgbSum = rgb[0] + rgb[1] + rgb[2];
-  rgbSum = rgbSum > 0 ? rgbSum : 1;
-  let overallBrightnessReduction = (255 * 3 / 10) / rgbSum;
-
-  let rgbNew = [];
-  for(let i = 0; i < 3; i++) {
-    let rgbSubtract = rgb[i] * overallBrightnessReduction;
-    rgbNew[i] = rgb[i] - rgbSubtract;
-  }
-
-  let newColor = `rgba(${rgbNew[0]},${rgbNew[1]},${rgbNew[2]}, ${rgb[3] + 0.1})`;
-  return newColor;
-  
 }
 
 // BLENDCOLOR FUNCTIONS
